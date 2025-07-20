9c239b6a5cb25eea3c169871dff3432a2d77d403
Deepika Sharma <sharmaparas23@gmail.com>
2025-07-20 16:17:27 +0530

    GET API CALL

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+#create the idea repository.This is where ideas will be stored
+
+ideas = {
+
+    1 : {
+
+        "id":1,
+        "idea_name":"ONDC",
+        "idea_description":"Details about ONDC",
+        "idea_author":"Adeeb"
+    },
+    2: {
+
+        "id":2,
+        "idea_name":"Save Soil",
+        "idea_description":"Details about Saving Soils",
+        "idea_author":"Ankit Sharma"
+    }
+}
+
+'''
+Create a Restful endpoint for fetching all the ideas
+'''
+@app.get("/ideaapp/api/v1/ideas")
+def get_all_ideas():
+    #Logic to fetch all the ideas
+    return ideas 
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(port=8080)
